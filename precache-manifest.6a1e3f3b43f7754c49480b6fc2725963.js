68824271a8c37efff95ab4a9278494fa073640d3
quyenvtb <quyenvo2511>
2023-07-11 10:38:22 +0700

    Updates

---
+++ precache-manifest.6a1e3f3b43f7754c49480b6fc2725963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4977e90c6531acd98f1909a6b0f78ad",
+    "url": "/cv-quyenvtb/index.html"
+  },
+  {
+    "revision": "28425192ee86d2da59df",
+    "url": "/cv-quyenvtb/static/css/main.f9028658.chunk.css"
+  },
+  {
+    "revision": "6875468116e923eed46a",
+    "url": "/cv-quyenvtb/static/js/2.3f95e2d8.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/cv-quyenvtb/static/js/2.3f95e2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28425192ee86d2da59df",
+    "url": "/cv-quyenvtb/static/js/main.fd241680.chunk.js"
+  },
+  {
+    "revision": "f8f73acabf203411b7bb",
+    "url": "/cv-quyenvtb/static/js/runtime-main.52b1757d.js"
+  }
+]);
